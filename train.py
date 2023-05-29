d300fa8f1b01d3b0aeb686a4bedcfe72c51b8921
quinn3111993 <nphuongquynh311@gmail.com>
2023-05-30 01:03:55 +0700

    .

--- train.py
+++ train.py
@@ -349,8 +349,10 @@ if __name__ == "__main__":
 
     size_valid_set = 0.1
     max_length = 512
-    num_epochs = 10
-    batch_size = 4
+    # num_epochs = 10
+    num_epochs = 1
+    # batch_size = 4
+    batch_size = 2
     gradient_accumulation_steps = 16
 
     # ADDED CODE
